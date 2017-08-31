845d753ee19fd9b4b4ac9c28ff3aa20a201d6622
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-08-30 18:43:30 -0700

    Finished Login UI

---
+++ smack/Controllers/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Alex Villacres on 8/30/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Alex Villacres on 8/30/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
