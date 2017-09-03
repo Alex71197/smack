150d1d3cdc0e4c1504e55e8ba63bb28ef8473a7b
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-09-03 10:23:02 -0700

    completed AvatarPickerUI

--- smack/Controllers/CreateAccountVC.swift
+++ smack/Controllers/CreateAccountVC.swift
@@ -28,6 +28,13 @@ class CreateAccountVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
+    
     @IBAction func createAccountPressed(_ sender: Any) {
         
         guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
@@ -53,7 +60,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func chooseAvatarPressed(_ sender: Any) {
-        
+        performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
     
     @IBAction func pickBGColorPressed(_ sender: Any) {
