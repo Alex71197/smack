6f81ebfef9c5d34778029b53189bac501f0f7b7a
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-09-12 21:14:08 -0700

    ADDED CHANNELS WRONG BRANCH OOPS

---
+++ smack/Models/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  smack
+//
+//  Created by Alex Villacres on 9/12/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Decodable {
+    
+    public private(set) var id: String!
+    public private(set) var channelName: String!
+    public private(set) var channelDescription: String!
+    
+}
---
+++ smack/Services/MessageService.swift
@@ -0,0 +1,43 @@
+//
+//  MessageService.swift
+//  smack
+//
+//  Created by Alex Villacres on 9/12/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+        
+                if let json = JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let description = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(id: id, channelName: name, channelDescription: description)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels)
+                    completion(true)
+                }
+                
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+}
